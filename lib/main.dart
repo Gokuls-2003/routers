23fe34619003dd2f26d6a177af9d9c4053257614
GOKUL SENTHIL <gokulsenthil899@gmail.com>
2024-05-02 10:13:07 +0530

    pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:routers/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +15,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("Go to Details")),
-      ),
+      home: HomePage(),
     );
   }
 }
